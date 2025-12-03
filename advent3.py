c28ba9daf9ae4014209142bbddfc3e7fd9c20496
Jacob Waddell <jwaddell1414@gmail.com>
2025-12-03 15:38:42 -0600

    Add solution for advent3 part 2

--- advent3.py
+++ advent3.py
@@ -7,11 +7,21 @@ def read_banks(filename: str):
             banks.append([int(num) for num in line.strip()])
     return banks
 
+def find_batteries_to_activate(bank: list[int], num_to_activate: int):
+    active_batteries = []
+    start_idx = 0
+    for num_remaining in range(num_to_activate, 0, -1):
+        bank_slice = bank[start_idx:(len(bank) - num_remaining + 1)]
+        battery = max(bank_slice)
+        start_idx += bank_slice.index(battery) + 1
+        active_batteries.append(battery)
+    return active_batteries
+
+REQUIRED_BATTERY_COUNT = 12
+
 if __name__ == "__main__":
     joltage = 0
     for bank in read_banks("advent3_input"):
-        start_battery = max(bank[:-1])
-        end_battery = max(bank[bank.index(start_battery) + 1:])
-        print(f"Joltage for bank {bank} is: {start_battery}{end_battery}")
-        joltage += (start_battery * 10) + end_battery
+        for idx, active_battery in enumerate(find_batteries_to_activate(bank, REQUIRED_BATTERY_COUNT)):
+            joltage += active_battery * 10 ** (REQUIRED_BATTERY_COUNT - idx - 1)
     print(f"The joltage is {joltage}")
