6985db54ebe9dc5fa51cdb23a008308f7feec0f5
Jacob Waddell <jwaddell1414@gmail.com>
2025-12-12 15:32:04 -0600

    Add solution for advent5 part 2

--- advent5.py
+++ advent5.py
@@ -35,14 +35,23 @@ if __name__ == '__main__':
         low_idx = find_insertion_idx(fresh_list, low)
         high_idx = find_insertion_idx(fresh_list, high)
 
+        # Edge case, if the high range number matches one already in the list then
+        # the existing number won't actually get removed (since list deletion doesn't include the high_idx).
+        # This number needs to be removed so that we don't get duplicate entries. Incrementing the index by 1
+        # will ensure this number is removed and no duplicates occur.
+        if high_idx < len(fresh_list) and high == fresh_list[high_idx]:
+            high_idx += 1
+        
         del fresh_list[low_idx:high_idx]
         if high_idx % 2 == 0:
             fresh_list.insert(low_idx, high)
         if low_idx % 2 == 0:
             fresh_list.insert(low_idx, low)
+    print(f"Fresh list: {fresh_list}")
 
     fresh_count = 0
-    for id in ids:
-        if find_insertion_idx(fresh_list, id) % 2 != 0:
-            fresh_count += 1
+    for low, high in zip(range(0, len(fresh_list) - 1, 2), 
+                        range(1, len(fresh_list), 2)):
+        print(f"Processing pair ({fresh_list[low]}, {fresh_list[high]})")
+        fresh_count += fresh_list[high] - fresh_list[low] + 1
     print(f"Number of fresh ids: {fresh_count}")
